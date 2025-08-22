3cbe2856659b169cbe790c4813cd80d50d0fbb32
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-08-22 10:00:43 +0200

    add initial code for task 1

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,96 @@ export type Student = {
   isActive?: boolean; // Optional value
 }
 
+// Array of student objects
+const students: Student[] = [
+  {
+    firstName: "Olivia",
+    lastName: "Smith",
+    age: 21,
+    courses: [101, 203, 305],
+    isActive: true,
+  },
+  {
+    firstName: "Liam",
+    lastName: "Johnson",
+    age: 19,
+    courses: [102, 202],
+    isActive: true,
+  },
+  {
+    firstName: "Emma",
+    lastName: "Williams",
+    age: 22,
+    courses: [101, 301, 404],
+    isActive: true,
+  },
+  {
+    firstName: "Noah",
+    lastName: "Brown",
+    age: 20,
+    courses: [203, 401, 402],
+    isActive: true,
+  },
+  {
+    firstName: "Ava",
+    lastName: "Jones",
+    age: 18,
+    courses: [101, 102],
+    isActive: true,
+  },
+  {
+    firstName: "William",
+    lastName: "Garcia",
+    age: 23,
+    courses: [305, 404, 501],
+    isActive: true,
+  },
+  {
+    firstName: "Sophia",
+    lastName: "Miller",
+    age: 21,
+    courses: [102, 202, 301],
+    isActive: true,
+  },
+  {
+    firstName: "James",
+    lastName: "Davis",
+    age: 19,
+    courses: [401, 502],
+    isActive: true,
+  },
+  {
+    firstName: "Isabella",
+    lastName: "Rodriguez",
+    age: 20,
+    courses: [101, 203, 301, 402],
+    isActive: true,
+  },
+  {
+    firstName: "Benjamin",
+    lastName: "Martinez",
+    age: 22,
+    courses: [202, 305, 501],
+    isActive: true,
+  }
+];
+
+type CourseStatus = "pending" | "active" | "finished";
+
+type Course = {
+  id: number;
+  name: string;
+  subject: string;
+  status: CourseStatus;
+}
+
+const course: Course = {
+    id: 101,
+    name: "TypeScript",
+    subject: "Web development",
+    status: "active"
+}
+
 function App() {
 
   // Primary - string, number, boolean, null undefined
@@ -30,7 +120,9 @@ function App() {
   return (
     <main>
     Students
-    <StudentList/>
+    <StudentList
+      students={students}
+    />
     </main>
   )
 }
--- src/StudentList.tsx
+++ src/StudentList.tsx
@@ -1,80 +1,10 @@
 import { type Student } from "./App";
 
-// Array of student objects
-const students: Student[] = [
-  {
-    firstName: "Olivia",
-    lastName: "Smith",
-    age: 21,
-    courses: [101, 203, 305],
-    isActive: true,
-  },
-  {
-    firstName: "Liam",
-    lastName: "Johnson",
-    age: 19,
-    courses: [102, 202],
-    isActive: true,
-  },
-  {
-    firstName: "Emma",
-    lastName: "Williams",
-    age: 22,
-    courses: [101, 301, 404],
-    isActive: true,
-  },
-  {
-    firstName: "Noah",
-    lastName: "Brown",
-    age: 20,
-    courses: [203, 401, 402],
-    isActive: true,
-  },
-  {
-    firstName: "Ava",
-    lastName: "Jones",
-    age: 18,
-    courses: [101, 102],
-    isActive: true,
-  },
-  {
-    firstName: "William",
-    lastName: "Garcia",
-    age: 23,
-    courses: [305, 404, 501],
-    isActive: true,
-  },
-  {
-    firstName: "Sophia",
-    lastName: "Miller",
-    age: 21,
-    courses: [102, 202, 301],
-    isActive: true,
-  },
-  {
-    firstName: "James",
-    lastName: "Davis",
-    age: 19,
-    courses: [401, 502],
-    isActive: true,
-  },
-  {
-    firstName: "Isabella",
-    lastName: "Rodriguez",
-    age: 20,
-    courses: [101, 203, 301, 402],
-    isActive: true,
-  },
-  {
-    firstName: "Benjamin",
-    lastName: "Martinez",
-    age: 22,
-    courses: [202, 305, 501],
-    isActive: true,
-  }
-];
+type StudentListProps = {
+    students: Student[]
+}
 
-export default function StudentList() {
+export default function StudentList({ students }: StudentListProps) {
   return (
     <table>
       <thead>
